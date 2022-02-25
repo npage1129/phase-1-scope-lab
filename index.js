45508330ad2da3c76d6015008c4898f93ad99488
nicolepage <npage1129@gmail.com>
2022-02-24 20:42:28 -0700

    commit tree c6384d838e47ac05b60252564ed83b7e74a85aed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'initial value';
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+return customerName;
+}
+function setBestCustomer(){
+bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+ leastFavoriteCustomer = 'new value';
+}
